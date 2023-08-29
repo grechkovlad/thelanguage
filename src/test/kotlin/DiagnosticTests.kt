9d12ddba0d6b539b9f069345e30f9090bbac7fc4
Vladislav Grechko <vladislavgrechko@yandex.ru>
2023-08-29 21:32:05 +0200

    Implement test generation

--- src/test/kotlin/DiagnosticTests.kt
+++ src/test/kotlin/DiagnosticTests.kt
@@ -1,45 +1,24 @@
-import ir.IrBuilder
-import parser.Parser
-import kotlin.reflect.cast
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.api.Nested
 
 class DiagnosticTests {
 
-    @Test
-    fun testIllegalReturnType() {
-        testOnExample("illegalReturnType")
-    }
-
-    @Test
-    fun returnFromVoid() {
-        testOnExample("returnFromVoid")
-    }
-
-    @Test
-    fun noReturn() {
-        testOnExample("noReturn")
-    }
-
-    private fun testOnExample(name: String) {
-        val srcText = readFromResources("/diagnostics/$name.lang")
-        val expectedCompilationError = retrieveExpectedDiagnostic(srcText, name)
-        val ast = Parser(srcText, name, true).parse()
-        var exceptionThrown = false
-        try {
-            IrBuilder(listOf(ast)).build()
-        } catch (t: Throwable) {
-            exceptionThrown = true
-            assert(expectedCompilationError.type.isInstance(t)) {
-                "Expected ${expectedCompilationError.type.simpleName}, got ${t::class.simpleName}"
-            }
-            assertEquals(
-                expectedCompilationError.location, expectedCompilationError.type.cast(t).location,
-                "Expected $expectedCompilationError, got $t"
-            )
+    @Nested
+    inner class Smoke{
+        @Test
+        fun testReturnFromVoid() {
+            runDiagnosticTest("/smoke/returnFromVoid")
         }
-        if (!exceptionThrown) {
-            assert(false) { "Expected ${expectedCompilationError.type.simpleName}, no diagnostic found" }
+
+        @Test
+        fun testIllegalReturnType() {
+            runDiagnosticTest("/smoke/illegalReturnType")
         }
+
+        @Test
+        fun testNoReturn() {
+            runDiagnosticTest("/smoke/noReturn")
+        }
+
     }
 }
--- src/test/kotlin/utils.kt
+++ src/test/kotlin/utils.kt
@@ -1,12 +1,18 @@
 import ir.CompilationError
+import ir.IrBuilder
 import lexer.*
+import parser.Parser
 import kotlin.reflect.KClass
+import kotlin.reflect.cast
+import kotlin.test.assertEquals
 
 fun readFromResources(path: String) = object {}.javaClass.enclosingClass.getResource(path)!!.readText()
 
-fun retrieveExpectedDiagnostic(srcText: String, fileName: String): ExpectedCompilationError {
+private fun retrieveExpectedDiagnostic(srcText: String, fileName: String): ExpectedCompilationError {
+
     fun Token.isAtToken() = this is KeySeq && type == KeySeqType.AT
     fun Token.isSharpToken() = this is KeySeq && type == KeySeqType.SHARP
+
     val lexer = LL3Lexer(srcText, DiagnosticMarkupSupportMode.SUPPORT)
     while (!lexer.current.isAtToken()) lexer.advance()
     lexer.advance()
@@ -26,3 +32,25 @@ fun retrieveExpectedDiagnostic(srcText: String, fileName: String): ExpectedCompi
 data class UnknownDiagnostic(val name: String) : RuntimeException()
 object EmptyDiagnostic : RuntimeException()
 data class ExpectedCompilationError(val type: KClass<out CompilationError>, val location: Location)
+
+fun runDiagnosticTest(path: String) {
+    val srcText = readFromResources("/diagnostics/$path.lang")
+    val expectedCompilationError = retrieveExpectedDiagnostic(srcText, path)
+    val ast = Parser(srcText, path, true).parse()
+    var exceptionThrown = false
+    try {
+        IrBuilder(listOf(ast)).build()
+    } catch (t: Throwable) {
+        exceptionThrown = true
+        assert(expectedCompilationError.type.isInstance(t)) {
+            "Expected ${expectedCompilationError.type.simpleName}, got ${t::class.simpleName}"
+        }
+        assertEquals(
+            expectedCompilationError.location, expectedCompilationError.type.cast(t).location,
+            "Expected $expectedCompilationError, got $t"
+        )
+    }
+    if (!exceptionThrown) {
+        assert(false) { "Expected ${expectedCompilationError.type.simpleName}, no diagnostic found" }
+    }
+}
